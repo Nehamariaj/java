6858bced5d17293a8c82992910e700ca7e5f5295
Nehamariaj <145547103+Nehamariaj@users.noreply.github.com>
2023-10-11 07:13:45 +0530

    AreaofShapes.java

--- AreaofShapes.java
+++ AreaofShapes.java
@@ -13,11 +13,13 @@ public class AreaofShapes {
 	public static void main(String[] args) {
 		Shape shape=new Shape();
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter the base and height of triangle:");
+		System.out.println("enter the base of triangle:");
 		float base=sc.nextFloat();
+		System.out.println("enter the height of triangle:");
 		float height=sc.nextFloat();
-		System.out.println("enter the length and breadth of rectangle:");
+		System.out.println("enter the length of rectangle:");
 		int length=sc.nextInt();
+		System.out.println("enter the breadth of rectangle:");
 		int breadth=sc.nextInt();
 		System.out.println("enter the radius of circle:");
 		float radius=sc.nextFloat();
@@ -31,12 +33,12 @@ public class AreaofShapes {
 class Shape{
 	
 	public static void area(int length,int breadth) {
-		System.out.println("Area of Rectangle with width="+length+" and breadth="+breadth+" is="+(length*breadth));
+		System.out.println("Area of Rectangle with width "+length+" and breadth "+breadth+" is="+(length*breadth));
 	}
 	public static void area(float height,float base) {
-		System.out.println("Area of triangle with base="+base+" and height="+height+" is="+(0.5*height*base));
+		System.out.println("Area of triangle with base "+base+" and height "+height+" is="+(0.5*height*base));
 	}
 	public static void area(float radius) {
-		System.out.println("Area of circle with radius="+radius+" is="+(3.14*radius*radius));
+		System.out.println("Area of circle with radius "+radius+" is="+(3.14*radius*radius));
 	}
 }
